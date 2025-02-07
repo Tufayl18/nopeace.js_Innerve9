61091fec5d0a86ee7631d0a952fde8d6da161ff6
Aditi Narkar <adunarkar2004@gmail.com>
2025-02-07 13:19:44 +0530

    override

--- app/page.js
+++ app/page.js
@@ -1,95 +1,79 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import styles from "../app/styles.module.css"
+import Image from "next/image"
+import bg from "./assets/bg3.jpeg"
+import logo from "./assets/logo3.png"
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const router = useRouter()
+  const [token, setToken] = useState("")
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const handleLogin = () => {
+    if (token) {
+      localStorage.setItem("githubAccessToken", token)
+      router.push("/dashboard")
+    } else {
+      alert("Please enter a GitHub access token.")
+    }
+  }
+
+  return (
+    <>
+      <main className={styles.main}>
+        <div className={styles.mainLeft}>
+          <div>
+            <a style={{ color: "#1ecbe1" }}>S</a>ta
+            <a style={{ color: "var(--divider)" }}>k</a>e.{" "}
+          </div>
+          <div>
+            S<a style={{ color: "var(--divider)" }}>o</a>lv
+            <a style={{ color: "#1ecbe1" }}>e</a>.{" "}
+          </div>
+          <div>
+            <a style={{ color: "#1ecbe1" }}>E</a>ar
+            <a style={{ color: "var(--divider)" }}>n</a>.{" "}
+          </div>
+        </div>
+        <div className={styles.mainRight}>
+          <div style={{ display: "flex", justifyContent: "Center" }}>
+            <Image src={logo} width={250} height={250} alt="GitStake Logo" />
+          </div>
+          Enter your GitHub Access Token <br />
+          <input
+            type="text"
+            value={token}
+            onChange={(e) => setToken(e.target.value)} // Update state on input change
+            style={{
+              color: "var(--divider)",
+              fontFamily: "var(--font-lucida)",
+              fontSize: 20,
+              height: 40,
+              width: "80%",
+              marginTop: 10,
+            }}
+          />
+          <button className={styles.LoginButton} onClick={handleLogin}>
+            Log In
+          </button>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      <Image
+        style={{
+          position: "absolute",
+          top: 100,
+          left: 60,
+          opacity: 0.15,
+          zIndex: 1,
+          overflow: "hidden",
+        }}
+        src={bg}
+        width={1300}
+        alt="Background"
+      />
+    </>
+  )
 }
+
